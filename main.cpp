7c99ceda71a2fab0ffc75e212c72c1b7cea96751
helric122915 <helric122915@yahoo.com>
2017-02-24 22:42:50 -0500

    Updates to the json parser

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
-#include "value.hpp"
+#include "parse.hpp"
 
 int main() {
+
+  std::string str = "\"hello\"  7";
+
+  char* F = &str[0];
+  char* L = &str[str.length() - 1];
+
+  value* val = parse(F,L);
+
+  val->print();
+  std::cout << '\n';
+
+  delete val;
+
+  /*
+  array_value *arr = new array_value();
+
+  arr->push_back(new bool_value(true));
+  arr->push_back(new number_value(7.5));
+
+  object_value *obj = new object_value();
+
+  obj->insert({"fun",arr});
+  obj->insert({"hello",arr});
+
+  std::cout << "Weight: " << obj->weight() << '\n';
+  */
 
   return 0;
 }
--- value.hpp
+++ value.hpp
@@ -9,9 +9,15 @@
 class value {
 public:
   virtual ~value() = default;
+  
+  virtual int weight() { return 1; }
+  virtual void print() = 0;
 };
 
-class null_value : public value {};
+class null_value : public value {
+public:
+  void print() { std::cout << "Null"; }
+};
 
 class bool_value : public value {
 private:
@@ -21,27 +27,78 @@ public:
   bool_value(bool b) : value(b) {}
 
   bool getValue() { return value; }
+  void print() { std::cout << "Bool: " << (value ? "true" : "false"); }
 };
 
 class string_value : public value, public std::string {
+public:
   using std::string::string;
+
+  void print() { std::cout << "String: " << *this; } 
 };
 
-class number_value : public value {
-private:
-  double value;
-
+class number_value : public value, public std::string {
 public:
-  number_value(double d) : value(d) {}
+  using std::string::string;
 
-  double getValue() { return value; }
+  void print() { std::cout << "String: " << *this; } 
 };
 
 class array_value : public value, public std::vector<value*> {
+public:
   using std::vector<value*>::vector;
+
+  ~array_value() {
+    for (std::vector<value*>::iterator it = this->begin(); it != this->end(); ++it) {
+      delete *it;
+    }
+  }
+
+  int weight() {
+    int total = 1;
+
+    for (std::vector<value*>::iterator it = this->begin(); it != this->end(); ++it) {
+      total += (*it)->weight();
+    }
+
+    return total;
+  }
+
+  void print() {
+    for (std::vector<value*>::iterator it = this->begin(); it != this->end(); ++it) {
+      std::cout << '[';
+      (*it)->print();
+      std::cout << ']';
+    }
+  }
 };
 
 class object_value : public value, public std::unordered_map<std::string,value*> {
+public:
   using std::unordered_map<std::string,value*>::unordered_map;
+
+  ~object_value() {
+    for (std::unordered_map<std::string,value*>::iterator it = this->begin(); it!= this->end(); ++it){
+      delete it->second;
+    }
+  }
+
+  int weight() {
+    int total = 1;
+    
+    for (std::unordered_map<std::string,value*>::iterator it = this->begin(); it!= this->end(); ++it){
+      total += (*it->second).weight();
+    }
+    return total;
+  }
+
+  void print() {
+    for (std::unordered_map<std::string,value*>::iterator it = this->begin(); it!= this->end(); ++it){
+      std::cout << '{' << it->first;
+      std::cout  << ',';
+      (*it->second).print();
+      std::cout << '}';
+    }
+  }
 };
 #endif
